30b31c75a1730496494996c392ace59067911962
Todorka Halacheva <todorka.halacheva@receipt-bank.com>
2022-12-09 15:37:47 +0200

    Day 9, part 2

--- 2022/day9.rb
+++ 2022/day9.rb
@@ -62,4 +62,92 @@ def part1(file)
   coords.size
 end
 
-p part1(file)
+# p part1(file)
+
+def part2(file)
+  tails = []
+  coords = []
+  for i in 0..9 do
+    tails << Point.new(0, 0)
+    coords << [Point.new(0, 0)]
+  end
+
+  file.each do |line|
+    direction, steps = line.chomp.split(' ')
+    case direction
+    when 'R'
+      for i in 1..steps.to_i do
+        tails[0].y += 1
+        coords[0] << Point.new(tails[0].x, tails[0].y)
+        for j in 1..9 do
+          move(tails[j - 1], tails[j], coords[j])
+        end
+      end
+    when 'L'
+      for i in 1..steps.to_i do
+        tails[0].y -= 1
+        coords[0] << Point.new(tails[0].x, tails[0].y)
+        for j in 1..9 do
+          move(tails[j - 1], tails[j], coords[j])
+        end
+      end
+    when 'U'
+      for i in 1..steps.to_i do
+        tails[0].x -= 1
+        coords[0] << Point.new(tails[0].x, tails[0].y)
+        for j in 1..9 do
+          move(tails[j - 1], tails[j], coords[j])
+        end
+      end
+    when 'D'
+      for i in 1..steps.to_i do
+        tails[0].x += 1
+        coords[0] << Point.new(tails[0].x, tails[0].y)
+        for j in 1..9 do
+          move(tails[j - 1], tails[j], coords[j])
+        end
+      end
+    end
+
+    # draw(coords)
+
+  end
+
+  coords.last.uniq.size
+end
+
+def move(head, tail, coords)
+  if !tail.adj?(head)
+    tail.x -= 1 if tail.x > head.x
+    tail.x += 1 if tail.x < head.x
+    tail.y -= 1 if tail.y > head.y
+    tail.y += 1 if tail.y < head.y
+    coords << Point.new(tail.x, tail.y)
+  end
+end
+
+def draw(coords)
+  arr = Array.new(30) { Array.new(30) }
+  for i in 0..29 do
+    for j in 0..29 do
+      arr[i][j] = '.'
+    end
+  end
+
+  arr[15][15] = 's'
+
+  coords.each_with_index do |c, i|
+    x = c.last.x
+    y = c.last.y
+    arr[x + 15][y + 15] = (i == 0 ? 'H' : i)
+  end
+
+  for i in 0..arr.length - 1 do
+    for j in 0..arr[i].length - 1 do
+      print arr[i][j]
+    end
+    puts ''
+  end
+end
+
+p part2(file)
