ed1f7f2b6025f42d983f19878e66eeedf2099e5a
Todorka Halacheva <todorka.halacheva@gmail.com>
2024-12-30 10:57:54 +0200

    WIP

--- 2024/day24.js
+++ 2024/day24.js
@@ -1,42 +1,34 @@
 const fs = require('fs');
 const input = fs.readFileSync('day24.txt', 'utf-8').trim().split('\n');
 
-const wires = new Map();
+const initialWires = new Map();
 const gates = [];
 
 for (const line of input) {
   if (line.includes(':')) {
     const [wire, value] = line.split(': ');
-    wires.set(wire, Number(value));
+    initialWires.set(wire, Number(value));
   } else if (line.includes(' -> ')) {
     const [gate, output] = line.split(' -> ');
     const [wire1, op, wire2] = gate.split(' ');
-    if (!wires.has(wire1)) wires.set(wire1, null);
-    if (!wires.has(wire2)) wires.set(wire2, null);
-    if (!wires.has(output)) wires.set(output, null);
+    if (!initialWires.has(wire1)) initialWires.set(wire1, null);
+    if (!initialWires.has(wire2)) initialWires.set(wire2, null);
+    if (!initialWires.has(output)) initialWires.set(output, null);
     gates.push({ wire1, op, wire2, output });
   }
 }
 
-function swap(a, b) {
-  let gateA = gates.find((g) => g.output === a);
-  let gateB = gates.find((g) => g.output === b);
-
-  gateA.output = b;
-  gateB.output = a;
-}
-
-function getSignal(wire) {
+function getSignal(wires, wire) {
   if (wires.get(wire) !== null) return wires.get(wire);
 
   const gate = gates.find((g) => g.output === wire);
   let signal;
   if (gate.op === 'AND') {
-    signal = getSignal(gate.wire1) & getSignal(gate.wire2);
+    signal = getSignal(wires, gate.wire1) & getSignal(wires, gate.wire2);
   } else if (gate.op === 'OR') {
-    signal = getSignal(gate.wire1) | getSignal(gate.wire2);
+    signal = getSignal(wires, gate.wire1) | getSignal(wires, gate.wire2);
   } else if (gate.op === 'XOR') {
-    signal = (getSignal(gate.wire1) ^ getSignal(gate.wire2)) >>> 0;
+    signal = getSignal(wires, gate.wire1) ^ getSignal(wires, gate.wire2);
   }
 
   wires.set(wire, signal);
@@ -57,7 +49,7 @@ function findNumber(wires, prefix) {
 
   for (const wire of wires.keys()) {
     if (wire.startsWith(prefix)) {
-      const bit = getSignal(wire);
+      const bit = getSignal(wires, wire);
       const power = Number(wire.slice(1));
       number += bit * (2 ** power);
     }
@@ -66,55 +58,159 @@ function findNumber(wires, prefix) {
   return number;
 }
 
+function generateSwaps(wires) {
+  const possible = [...wires.keys()].filter(w => !w.startsWith('x') && !w.startsWith('y'));
+
+  const swaps = [];
+  for (let i = 0; i < possible.length; i++) {
+    for (let j = i + 1; j < possible.length; j++) {
+      swaps.push([possible[i], possible[j]]);
+    }
+  }
+
+  return swaps;
+}
+
+function swap(a, b) {
+  let gateA = gates.find((g) => g.output === a);
+  let gateB = gates.find((g) => g.output === b);
+
+  gateA.output = b;
+  gateB.output = a;
+}
+
+function check() {
+  for (let i = 0; i < 10; i++) {
+    const wires = randomizeWires(initialWires);
+    const x = findNumber(wires, 'x');
+    const y = findNumber(wires, 'y');
+    const z = findNumber(wires, 'z');
+    if (x + y !== z) return false;
+  }
+
+  return true;
+}
+
+function findSwaps(swaps) {
+  for (let i = 0; i < swaps.length; i++) {
+    for (let j = i + 1; j < swaps.length; j++) {
+      for (let k = j + 1; k < swaps.length; k++) {
+        for (let l = k + 1; l < swaps.length; l++) {
+          const set = new Set([...swaps[i], ...swaps[j], ...swaps[k], ...swaps[l]]);
+          if (set.size !== 8) continue;
+          if ([...set].filter(s => s.startsWith('z')).length < 3) continue;
+
+          try {
+            swap(...swaps[i]);
+            swap(...swaps[j]);
+            swap(...swaps[k]);
+            swap(...swaps[l]);
+
+            if (check()) {
+              console.log('Found solution: ', swaps[i], swaps[j], swaps[k], swaps[l]);
+              console.log([...swaps[i], ...swaps[j], ...swaps[k], ...swaps[l]].sort().join());
+            }
+          } catch (e) {
+            continue;
+          } finally {
+            swap(...swaps[i]);
+            swap(...swaps[j]);
+            swap(...swaps[k]);
+            swap(...swaps[l]);
+          }
+        }
+      }
+    }
+  }
+}
+
+function findWrongBits(wires, x, y) {
+  const z = findNumber(wires, 'z');
+  const result = dec2bin(z).split('').reverse().join('');
+  const expected = dec2bin(x + y).split('').reverse().join('') + '0';
+
+  const wrongBits = [];
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] !== expected[i]) wrongBits.push('z' + i);
+  }
+
+  return wrongBits;
+}
+
+function getRandomBit() {
+  return Math.floor(Math.random() * 2);
+}
+
+function randomizeWires(wires) {
+  const random = new Map(wires);
+  for (const wire of random.keys()) {
+    if (wire.startsWith('x') || wire.startsWith('y')) {
+      random.set(wire, getRandomBit());
+    }
+  }
+
+  return random;
+}
+
+function findGoodSwaps(startWires, allSwaps) {
+  let wires = new Map(startWires);
+  const x = findNumber(wires, 'x');
+  const y = findNumber(wires, 'y')
+  const wrongWires = findWrongBits(wires, x, y);
+
+  if (wrongWires.length < 5) return allSwaps;
+
+  const goodSwaps = [];
+  for (const s of allSwaps) {
+    try {
+      swap(...s);
+      const wrongWires2 = findWrongBits(new Map(startWires), x, y);
+      if (wrongWires2.length < wrongWires.length) goodSwaps.push(s);
+    } catch (e) {
+      continue;
+    } finally {
+      swap(...s);
+    }
+  }
+
+  return [...goodSwaps];
+}
+
 function findWrongWires(wires) {
   const wrongGates = gates.filter(g => wires.includes(g.output));
   if (wrongGates.length === 0) return [];
 
-  const newWires = wrongGates.map(g => g.wire1).concat(wrongGates.map(g => g.wire2));
+  const newWires = wrongGates.map(g => g.wire1).concat(wrongGates.map(g => g.wire2)).
+                    filter(w => !wires.startsWith('x') && !wires.startsWith('y'));
 
-  return [...wires, ...newWires, ...findWrongWires(newWires)];
+  return new Set([...wires, ...newWires, ...findWrongWires(newWires)]);
 }
 
-function check() {
-  const x = findNumber(wires, 'x');
-  const y = findNumber(wires, 'y');
-  const z = findNumber(wires, 'z');
+let wires = new Map(initialWires);
+const allSwaps = generateSwaps(wires);
 
-  return x + y === z;
+let goodSwaps = findGoodSwaps(wires, allSwaps);
+console.log('Initially found ', goodSwaps.length);
+
+for (let i = 0; i < 5; i++) {
+  wires = randomizeWires(initialWires);
+  goodSwaps = findGoodSwaps(wires, goodSwaps);
+  console.log('Iteration ', i, goodSwaps.length);
 }
+
+console.log(goodSwaps);
+console.log(goodSwaps.length);
+
+findSwaps(goodSwaps);
 
 // const x = findNumber(wires, 'x');
 // const y = findNumber(wires, 'y');
+// swap('bss', 'grr')
+// swap('crp', 'tnn' )
+// swap('gkk', 'tjk')
+// swap('z16', 'fvv')
 // const z = findNumber(wires, 'z');
-
-// console.log('0' + dec2bin(x + y));
+// console.log(z);
+// console.log(x + y);
 // console.log(dec2bin(z));
-// 45 total
-// 31  30          19    15
-// 0   10001001101 1000  0100011011100110
-// 1   10001001101 0111  0100011011100110
-
-swap('z31', 'z19');
-// swap('z18', 'bss');
-// swap('cpr', 'wgk');
-
-const wrongWires = findWrongWires(['z18', 'z17', 'z16']).filter(w => !w.startsWith('x') && !w.startsWith('y'));
-const unique = [...new Set(wrongWires)];
-console.log(unique.join(','));
-console.log(unique.length);
-const mapXY = new Map(unique.map(w => [w, null]));
-for (const wire of unique) {
-  const gate = gates.find(g => g.output === wire);
-  if ((gate.wire1.startsWith('x') || gate.wire1.startsWith('y')) && (gate.wire2.startsWith('x') || gate.wire2.startsWith('y'))) {
-    mapXY.set(wire, getSignal(wire));
-  }
-}
-
-console.log(mapXY);
-
-const x = findNumber(wires, 'x');
-const y = findNumber(wires, 'y');
-const z = findNumber(wires, 'z');
-
-console.log(dec2bin(x + y));
-console.log(dec2bin(z));
+// console.log(dec2bin(x + y));
